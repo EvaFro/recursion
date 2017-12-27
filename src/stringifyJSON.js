7f1c8cc5020a63c75f21fb731ff2058af3ee12c3
EvaFro <frolli.studios@gmail.com>
2017-12-26 21:43:53 -0700

    Completed stringifyJSON - passed all tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,27 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+   if (Array.isArray(obj)) {
+    obj = obj.map(function(item) {
+      return stringifyJSON(item);
+    });
+    return '[' + obj + ']';
+  }
+  
+  if (obj && typeof(obj) === 'object') {
+    var results = [];
+    for (var key in obj) {
+      if (obj[key] === undefined || typeof(obj[key]) === 'function') {
+        continue;
+      }
+      results.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+    return '{' + results.join() + '}';
+  }
+  
+  if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  }
+
+  return '' + obj;
 };
